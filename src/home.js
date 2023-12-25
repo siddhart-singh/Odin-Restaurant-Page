4189ffe6a113dd274db96d9b1a79cb037951ad6a
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-26 01:17:28 +0530

    Compete home.js

--- src/home.js
+++ src/home.js
@@ -8,6 +8,9 @@ import YellowHomeBG from "./image/yellowHomeBG.jpg";
 import HomeBannerA from "./image/homeBannerA.jpg";
 import HomeBannerB from "./image/homeBannerB.jpg";
 import HomeBannerC from "./image/homeBannerC.jpg";
+import pizza from "./image/pizza.jpg";
+import pasta from "./image/pasta.jpg";
+import burger from "./image/burger.jpg";
 
 export default function home() {
   document.querySelectorAll(".nav-btn").forEach((btn) => {
@@ -36,28 +39,70 @@ export default function home() {
   });
 
   const pageRed = createEl("div", ["page"], "", pageBanner);
-  const pageRedImg = createEl("div", ["page-img", "page-red"], "", pageRed, {});
+  const pageRedImgDiv = createEl(
+    "div",
+    ["page-img-div", "page-red"],
+    "",
+    pageRed,
+    {},
+  );
+  const pageRedImg = createEl(
+    "img",
+    ["page-img", "pasta-img"],
+    "",
+    pageRedImgDiv,
+    { src: pasta },
+  );
+  const pageRedP = createEl(
+    "p",
+    ["page-para", "pasta"],
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque inventore esse ex blanditiis repellat itaque!",
+    pageRedImgDiv,
+  );
 
   const pageBlue = createEl("div", ["page"], "", pageBanner);
-  const pageBlueImg = createEl(
+  const pageBlueImgDiv = createEl(
     "div",
-    ["page-img", "page-blue"],
+    ["page-img-div", "page-blue"],
     "",
     pageBlue,
     {},
   );
-
+  const pageBlueImg = createEl(
+    "img",
+    ["page-img", "burger-img"],
+    "",
+    pageBlueImgDiv,
+    { src: burger },
+  );
+  const pageBlueP = createEl(
+    "p",
+    ["page-para", "burger"],
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum laboriosam quaerat perferendis quam maiores possimus?",
+    pageBlueImgDiv,
+  );
 
   const pageYellow = createEl("div", ["page"], "", pageBanner);
-  const pageYellowImg = createEl(
+  const pageYellowImgDiv = createEl(
     "div",
-    ["page-img", "page-yellow"],
+    ["page-img-div", "page-yellow"],
     "",
     pageYellow,
     {},
   );
-  console.log(pageYellowImg);
-  
+  const pageYellowImg = createEl(
+    "img",
+    ["page-img", "pizza-img"],
+    "",
+    pageYellowImgDiv,
+    { src: pizza },
+  );
+  const pageYellowP = createEl(
+    "p",
+    ["page-para", "pizza"],
+    "Lorem ipsum dolor sit amet.",
+    pageYellowImgDiv,
+  );
 
   return [pageBanner, pageRed, pageBlue, pageYellow];
 }
