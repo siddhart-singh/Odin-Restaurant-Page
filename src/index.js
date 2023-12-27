a6b3967f1513ae1d9120de7574135179c23c1f98
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-27 16:55:16 +0530

    add navbtn switching

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ import homepage from "./homepage.js";
 import home from "./home.js";
 import menu from "./menu.js";
 import "./input.css";
+import { doc } from "prettier";
 
+let body, content, activePage;
 window.addEventListener("load", (e) => {
-  let body = document.querySelector("body");
+  body = document.querySelector("body");
   body.classList.add("body");
   body.append(...homepage());
 
-  let content = document.querySelector("#content");
+  content = document.querySelector("#content");
   content.classList.add("content");
+  content.append(...home());
+  activePage = "home";
 
-  content.append(...menu());
+  const navBar = document.querySelectorAll(".nav-btn");
+  navBar.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if (e.target.dataset.id == "home" && activePage != "home") {
+        content.append(...home());
+        activePage = "home";
+      } else if (e.target.dataset.id == "menu" && activePage != "menu") {
+        content.append(...menu());
+        activePage = "menu";
+      } else if (e.target.dataset.id == "contact" && activePage != "contact") {
+        console.log("HELLO");
+      }
+    });
+  });
 });
--- src/switchActiveTab.js
+++ src/switchActiveTab.js
@@ -1,8 +1,9 @@
 export default function switchTab(tab) {
   document.querySelectorAll(".nav-btn").forEach((btn) => {
     if (btn.getAttribute("data-id") === `${tab}`) {
-      console.log(btn);
       btn.classList.add(`active-${tab}`);
+    }else{
+      btn.classList.remove(`active-${btn.dataset.id}`);
     }
   });
 }
