84d9ca785268917732d07cfa17e4cf1e1cf3b7b9
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-29 21:43:15 +0530

    Complete contact.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
           "4px 4px rgb(34 197 94),5px 5px rgb(0,0,0), 5px 3px rgb(0,0,0), 3px 5px rgb(0,0,0)",
         btnMenu:
           "4px 4px rgb(234 179 8), 5px 5px rgb(0,0,0), 5px 3px rgb(0,0,0), 3px 5px rgb(0,0,0)",
+        btnContact:
+          "4px 4px rgb(249 115 22), 5px 5px rgb(0,0,0), 5px 3px rgb(0,0,0), 3px 5px rgb(0,0,0)",
+        formShadow: "12px 12px 0 8px rgb(0,0,0)",
       },
       fontFamily: {
         kalnia: "'Kalnia', serif",
